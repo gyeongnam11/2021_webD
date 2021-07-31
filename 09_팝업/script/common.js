bb4e8da03673cdfb5bbf2acf9a30deaadbb902c0
gyeongnam11 <rrudska11@gmail.com>
2021-08-01 00:58:35 +0900

    가로메뉴팝업구현 중

---
+++ 09_팝업/script/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $(".shortcut").click(function () {
+        $(".layer").show();
+        $(".layer-bg").show();
+       
+    });
+
+    $(".close-btn").click(function () {
+        $(".layer").hide();
+        $(".layer-bg").hide();
+        
+    });
+});
