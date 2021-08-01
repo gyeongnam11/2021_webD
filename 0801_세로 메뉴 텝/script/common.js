3c310d488f9ed43ebfe613b71fca43cb967a68e5
gyeongnam11 <rrudska11@gmail.com>
2021-08-02 01:10:40 +0900

    세로 메뉴 텝

---
+++ 0801_세로 메뉴 텝/script/common.js
@@ -0,0 +1,29 @@
+//side menu
+$(document).ready(function () {
+
+    $(".menu > ul > li").mouseover(function () {
+        $(this).find(".submenu").stop().slideDown(200);
+    });
+
+    $(".menu > ul > li").mouseout(function () {
+        $(this).find(".submenu").stop().slideUp(200);
+    });
+
+        
+});
+
+//popup
+$(document).ready(function () {
+
+    $(".banner").click(function () {
+        $(".layer").show();
+        $(".layer-bg").show();
+       
+    });
+
+    $(".close-btn").click(function () {
+        $(".layer").hide();
+        $(".layer-bg").hide();
+        
+    });
+});
